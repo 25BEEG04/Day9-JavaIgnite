d00233fa709f3fc5af5e8717ad8199e3182e8a3c
rite2harini <rite2harini@gmail.com>
2026-06-04 13:07:28 +0530

    Create Word_Reversal_Sentence.java

---
+++ Word_Reversal_Sentence.java
@@ -0,0 +1,11 @@
+/*
+Word Reversal in Sentence
+
+Create a program that:
+
+Takes a full sentence as input
+Reverses each word, but keeps word order same
+Example:
+Input: Java is fun
+Output: avaJ si nuf
+  */
