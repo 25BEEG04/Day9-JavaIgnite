237b7982f5e01b260947a38f0b0d2389f5005461
rite2harini <rite2harini@gmail.com>
2026-06-04 13:05:46 +0530

    Create TextProcessorSystem.java

---
+++ TextProcessorSystem.java
@@ -0,0 +1,22 @@
+/*
+Create a program that:
+
+Takes a string input using StringBuilder
+
+Perform the following operations:
+
+Tasks:
+Append a word at the end
+Insert a word in the middle
+Replace part of the string
+Reverse the final string
+Example Flow:
+Input: Java
+Append: Programming
+Insert: "Easy" in middle
+Replace: "Easy" → "Powerful"
+Reverse final string
+Rules:
+Must use StringBuilder only
+Do not convert back to String for operations
+*/
