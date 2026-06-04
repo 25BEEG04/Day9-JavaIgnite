dcd0baca9184ecc1d1cb3509228b8e24a757b2a3
rite2harini <rite2harini@gmail.com>
2026-06-04 13:06:47 +0530

    Create First_Non_Repeating_Character.java

---
+++ First_Non_Repeating_Character.java
@@ -0,0 +1,14 @@
+/*
+First Non-Repeating Character
+
+Create a program that:
+
+Takes a string input
+Finds the first character that does NOT repeat
+Example:
+Input: programming
+Output: p
+Hint:
+
+You need frequency logic using loops.
+*/
